bb2b0e779ad3172078aee825d333cff07dc021b6
Connor Farnworth <connor.farnworth@gmail.com>
2018-03-05 17:33:28 -0600

    Update font, start a better show/hide script, fixes stuff

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
 function toggleContact() {
-    var x = document.getElementById("contactInfo");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+  var x = document.getElementById("contactInfo");
+  var isOpen = false;
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    isOpen = "false";
+  } else {
+    x.style.display = "block";
+    isOpen = "true";
+  }
 }
 
 function toggleWindow() {
-    var x = document.getElementById("contactInfo");
-    var y = document.getElementById("totalWindow");
-    if (y.style.display == "block") {
-        y.style.display = "none";
-        x.style.display = "none";
-    } else {
-        y.style.display = "block";
-    }
+  var x = document.getElementById("contactInfo");
+  var y = document.getElementById("totalWindow");
+  var isOpen = false;
+
+  if (y.style.display == "block" && x.style.display == "none") {
+    y.style.display = "none";
+    x.style.display = "none";
+    isOpen = false;
+  }
+  else if(y.style.display == "block" && x.style.display == "block") {
+    y.style.display = "none";
+    x.style.display = "none";
+    isOpen = true;
+
+  }
+  else if(y.style.display == "none" && x.style.display == "none" && isOpen == true) {
+    y.style.display = "block";
+    x.style.display = "block";
+    isOpen = false;
+  }
+
+
 }
