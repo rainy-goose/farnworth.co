0b458374bdd4f8db54a3d4e2588f3b3e34d3867f
Connor Farnworth <connor.farnworth@gmail.com>
2018-03-07 07:22:17 -0600

    Updates minor index stuff and fixes a script

--- js/script.js
+++ js/script.js
@@ -4,34 +4,24 @@ function toggleContact() {
 
   if (x.style.display === "block") {
     x.style.display = "none";
-    isOpen = "false";
+    isOpen = false;
   } else {
     x.style.display = "block";
-    isOpen = "true";
+    isOpen = true;
   }
 }
 
 function toggleWindow() {
-  var x = document.getElementById("contactInfo");
-  var y = document.getElementById("totalWindow");
-  var isOpen = false;
+    var x = document.getElementById("contactInfo");
+    var y = document.getElementById("totalWindow");
+    var isOpen = false;
 
-  if (y.style.display == "block" && x.style.display == "none") {
-    y.style.display = "none";
-    x.style.display = "none";
-    isOpen = false;
-  }
-  else if(y.style.display == "block" && x.style.display == "block") {
-    y.style.display = "none";
-    x.style.display = "none";
-    isOpen = true;
-
-  }
-  else if(y.style.display == "none" && x.style.display == "none" && isOpen == true) {
-    y.style.display = "block";
-    x.style.display = "block";
-    isOpen = false;
-  }
-
+    if (y.style.display === "block") {
+      y.style.display = "none";
+      x.style.display = "none";
+    }
+    else {
+      y.style.display = "block";
+    }
 
 }
