25c7f5760eb0b75d9784114f266a127ca7b9af9b
Connor Farnworth <connor.farnworth@gmail.com>
2018-03-09 07:39:41 -0600

    Comments scripts

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ function toggleContact() {
   var x = document.getElementById("contactInfo");
 
   if (x.style.display === "block") {
+    // if contact is open, hide contact
     x.style.display = "none";
   } else {
+    // otherwise show contact
     x.style.display = "block";
   }
 }
@@ -13,10 +15,12 @@ function toggleWindow() {
     var y = document.getElementById("totalWindow");
 
     if (y.style.display === "block") {
+      // if window is open, hide window and contact
       y.style.display = "none";
       x.style.display = "none";
     }
     else {
+      // otherwise show window
       y.style.display = "block";
     }
 
@@ -26,8 +30,10 @@ function toggle404() {
   var z = document.getElementById("404text");
 
   if (z.style.display === "block") {
+    // if 404 is open, hide 404
     z.style.display = "none";
   } else {
+    // otherwise show 404
     z.style.display = "block";
   }
 }
